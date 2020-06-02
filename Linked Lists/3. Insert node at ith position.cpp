db029114119203175debc512692ead159d2b018c
Ishan Pandey <Ishan.parasar.3359@gmail.com>
2020-06-02 17:42:36 +0530

    Update 3. Insert node at ith position.cpp

--- Linked Lists/3. Insert node at ith position.cpp
+++ Linked Lists/3. Insert node at ith position.cpp
@@ -1,6 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+ Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+
+
+
 /*
 to insert a new node in ll, create a  new node (data, next=null)
 and shift nexts easily bro, but remember not to lose next pointer address
